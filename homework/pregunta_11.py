54d750d4dfdd05a2ed5f1ee38789efb430d333df
Elpulpo888 <flopera@unal.edu.co>
2025-01-06 16:42:31 -0500

    lab2Done

--- homework/pregunta_11.py
+++ homework/pregunta_11.py
@@ -4,7 +4,10 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+import  pandas as pd 
+tbl0= pd.read_csv("files/input/tbl0.tsv", sep= "\t")
+tbl1= pd.read_csv("files/input/tbl1.tsv", sep= "\t")
+tbl2= pd.read_csv("files/input/tbl2.tsv", sep= "\t")
 
 def pregunta_11():
     """
@@ -22,3 +25,8 @@ def pregunta_11():
     38   38      d,e
     39   39    a,d,f
     """
+
+    z = tbl1.copy()
+    tabla_agrupada = z.groupby("c0")["c4"].apply(lambda x: ",".join(sorted(x))).reset_index()
+    df = pd.DataFrame(tabla_agrupada)
+    return df
